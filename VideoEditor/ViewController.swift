6c93f547c41abcbfa7303f6086cdb1a61e11a2c6
Mobdev125 <mobdev125@mobdev125s-imac.local>
2017-06-02 22:12:02 +0200

    Initial Commit

---
+++ VideoEditor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VideoEditor
+//
+//  Created by Mobdev125 on 6/2/17.
+//  Copyright © 2017 Mobdev125. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
